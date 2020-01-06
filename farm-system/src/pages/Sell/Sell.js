bcaee22aa738d076de25c7fae4b22edb9bab9cdb
wangguan0521 <56301854+wangguan0521@users.noreply.github.com>
2020-01-06 09:19:12 +0800

    '20200106wangguan提交测试'

--- farm-system/src/pages/Sell/Sell.js
+++ farm-system/src/pages/Sell/Sell.js
@@ -4,7 +4,7 @@ class Sell extends Component{
     render(){
         return(
             <div className='APP'>
-                这是销售管理,可以从作物的销售品种(大豆、花生...)、销售吨数、销售时间等入手
+                这是王冠的销售管理,可以从作物的销售品种(大豆、花生...)、销售吨数、销售时间等入手
             </div>
         )
     }
